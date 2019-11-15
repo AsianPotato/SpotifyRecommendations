bbe00b6e0f0c573c81b1a7531668684734385f7f
RoxazX <48754551+RoxazX@users.noreply.github.com>
2019-11-15 16:14:43 +0000

    Add files via upload

--- SpotifyRecommendations/Program.cs
+++ SpotifyRecommendations/Program.cs
@@ -5,6 +5,7 @@ using SpotifyAPI.Web;
 using SpotifyAPI.Web.Auth;
 using SpotifyAPI.Web.Enums;
 using SpotifyAPI.Web.Models;
+using System.Globalization;
 
 namespace SpotifyRecommendations
 {
@@ -17,10 +18,13 @@ namespace SpotifyRecommendations
             Console.ReadLine();
 
         }
-        public  static async void Example()
+        public static void Example()
         {
+            //getting country of user
+            string name = RegionInfo.CurrentRegion.EnglishName;
+            Console.WriteLine(name);
 
-                var auth = new ImplicitGrantAuth(
+            var auth = new ImplicitGrantAuth(
                   "7f08980f1dae4f3d98a40d44ef235b03",
                   "http://localhost:4002",
                   "http://localhost:4002",
@@ -34,30 +38,33 @@ namespace SpotifyRecommendations
                         TokenType = payload.TokenType,
                         AccessToken = payload.AccessToken
                     };
+                    // FeaturedPlaylists playlists = api.GetFeaturedPlaylists();
+                    //Console.WriteLine(playlists.Message);
+                    //playlists.Playlists.Items.ForEach(playlist => Console.WriteLine(playlist.Name));
+
+                    //getting playlists from categories and outputting names and links of the playlist.
+                    CategoryPlaylist playlists = api.GetCategoryPlaylists("party");
+                    playlists.Playlists.Items.ForEach(playlist => Console.WriteLine("Playlist Name: " + playlist.Name + ",\nLink: " + playlist.Uri));
+
                     // Do requests with API client
-                    var newsapiresults = await NewsApi.SearchByKeyword("bitcoin");
+                    var newsapiresults = await NewsApi.SearchByKeyword("bitcoin", name);
                     if (newsapiresults == null)
                         return;
                     Debug.WriteLine(newsapiresults);
                     Debug.WriteLine(newsapiresults.totalResults);
                     foreach (var result in newsapiresults.articles)
                     {
-                        Debug.WriteLine(result.description);
+                        //Console.WriteLine();
+                        Debug.WriteLine(result.title);
                     }
                     var spotifyresults = await api.SearchItemsAsync("drake", SearchType.All);
                     if (spotifyresults == null)
                         return;
 
-                   
+                    Console.WriteLine(spotifyresults);
                 };
                 auth.Start(); // Starts an internal HTTP Server
-                auth.OpenBrowser();
-            
+                auth.OpenBrowser();          
         }
-
-
-
     }
-
-
 }
